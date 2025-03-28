91091fbaba2369c2c402b6ef209943f1e8a7e5fb
ILYES <ILYES@OnTheFlux>
2025-03-28 17:16:34 +0100

    segundo coche añadido (honda)

--- controle_de_versiones/src/Tarea2/crearCoche.java
+++ controle_de_versiones/src/Tarea2/crearCoche.java
@@ -6,7 +6,8 @@ public class crearCoche {
 	        Coche coche1 = new Coche("Toyota", "Corolla", 2022);
 	        coche1.mostrarInformacion();
 
-	       
+	        Coche coche2 = new Coche("Honda", "Civic", 2021);
+	        coche2.mostrarInformacion();
 	    }
 	
 	}
